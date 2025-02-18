21425b68175757d3e0dbf538da7532de416aaa72
Bruna Freitas Almeida <bubufreitas@hotmail.com>
2025-02-18 16:58:40 -0300

    Create api get municipios

---
+++ municipios-api/municipios-api/Controllers/MunicipiosController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using municipios_api.Models;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore.InMemory;
+
+namespace municipios_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MunicipiosController : Controller
+    {
+        private readonly ILogger<MunicipiosController> _logger;
+        private readonly string _url = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/MG/municipios";
+
+        public MunicipiosController(ILogger<MunicipiosController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    List<Municipio> municipios = new List<Municipio>();
+                    HttpResponseMessage response = await client.GetAsync(_url);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string jsonResponse = await response.Content.ReadAsStringAsync();
+
+                        // Desserializa a resposta para uma lista de objetos anônimos
+                        municipios = JsonSerializer.Deserialize<List<Municipio>>(jsonResponse, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                        if(municipios.Count > 0)
+                        {
+                            _logger.LogInformation($"Sucesso em buscar municípios, total de: {municipios.Count} municípios encontrados.");
+                            return Ok(municipios);
+                        }
+
+                        _logger.LogInformation("Nenhum município encontrado!");
+                        return NotFound();
+                    }
+                    else
+                    {                        
+                        _logger.LogError($"Erro: {response.StatusCode} - GET() in MunicipiosController");
+                        return NoContent();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"Erro ao buscar municípios: {ex.Message} - GET() in MunicipiosController");
+                    return BadRequest(ex);
+                }
+            };
+        }
+    }
+}
---
+++ municipios-api/municipios-api/Models/Municipio.cs
@@ -0,0 +1,8 @@
+﻿namespace municipios_api.Models
+{
+    public class Municipio
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }        
+    }
+}

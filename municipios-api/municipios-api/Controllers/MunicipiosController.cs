e256d612af5caf1aba8ae5a8b27822db0eac9cfa
Bruna Freitas Almeida <bubufreitas@hotmail.com>
2025-02-24 18:42:17 -0300

    Created methode Put, Delete All and Delete By Id

--- municipios-api/municipios-api/Controllers/MunicipiosController.cs
+++ municipios-api/municipios-api/Controllers/MunicipiosController.cs
@@ -136,7 +136,7 @@ namespace municipios_api.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public IActionResult Get(int id)
+        public IActionResult GetById(int id)
         {
             using (HttpClient client = new HttpClient())
             {
@@ -163,7 +163,153 @@ namespace municipios_api.Controllers
                 }
                 catch (Exception ex)
                 {
-                    _logger.LogError($"Erro ao buscar município: {ex.Message} - GET() in MunicipiosController");
+                    _logger.LogError($"Erro ao buscar município: {ex.Message} - GETBYID() in MunicipiosController");
+                    return BadRequest(ex);
+                }
+            };
+        }
+
+        /// <summary>
+        /// Atualiza os dados do município.
+        /// </summary>
+        /// <param name="id">Número de identificação do município.</param>
+        /// <returns>Dados atualizados com sucesso.</returns>
+        /// <response code="200">Dados atualizados no banco de dados.</response>
+        /// <response code="404">Nenhum dado encontrado no banco de dados.</response>
+        /// <response code="400">Erro na requisição.</response>
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Put(int id, Municipio municipio)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    var options = new DbContextOptionsBuilder<ApplicationContext>()
+                                .UseInMemoryDatabase(databaseName: _dataBaseName)
+                                .Options;
+
+                    using (var context = new ApplicationContext(options))
+                    {
+                        // Busca um município por id no banco de dados
+                        var municipioExistente = context.Municipios.Find(id);
+
+                        if (municipioExistente == null)
+                        {
+                            _logger.LogInformation($"Nenhum município encontrado com o ID: {id}.");
+                            return NotFound();
+                        }
+
+                        // Se o município for encontrado, efetua a atualização
+                        context.Entry(municipioExistente).CurrentValues.SetValues(municipio);
+                        context.SaveChanges();
+
+                        _logger.LogInformation($"Sucesso ao atualizar município: {municipioExistente.Nome}.");
+                        return Ok(municipioExistente);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"Erro ao atualizar município: {ex.Message} - PUT() in MunicipiosController");
+                    return BadRequest(ex);
+                }
+            };
+        }
+
+
+        /// <summary>
+        /// Deleta todos municípios persistidos no banco de dados.
+        /// </summary>
+        /// <returns>Não há retorno.</returns>
+        /// <response code="204">Todos os municípios foram deletados com sucesso do banco de dados.</response>
+        /// <response code="404">Nenhum dado encontrado no banco de dados.</response>
+        /// <response code="400">Erro na requisição.</response>
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Delete()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    var options = new DbContextOptionsBuilder<ApplicationContext>()
+                                .UseInMemoryDatabase(databaseName: _dataBaseName)
+                                .Options;
+
+                    using (var context = new ApplicationContext(options))
+                    {
+                        // Busca todos os municípios persistidos no banco de dados
+                        var municipios = context.Municipios.ToList();
+
+                        if (!municipios.Any())
+                        {
+                            _logger.LogInformation("Nenhum município encontrado para deletar.");
+                            return NotFound();
+                        }
+
+                        // Se existir algum município na base, efetua a deleção
+                        context.Municipios.RemoveRange(municipios);
+                        context.SaveChanges();                        
+
+                        _logger.LogInformation($"Sucesso ao deletar todos municípios.");
+                        return NoContent();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"Erro ao deletar todos municípios: {ex.Message} - DELETE() in MunicipiosController");
+                    return BadRequest(ex);
+                }
+            };
+        }
+
+        /// <summary>
+        /// Deleta município por id.
+        /// </summary>
+        /// <returns>Não há retorno.</returns>
+        /// <response code="204">Município deletado com sucesso do banco de dados.</response>
+        /// <response code="404">Nenhum dado encontrado no banco de dados.</response>
+        /// <response code="400">Erro na requisição.</response>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult DeleteById(int id)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    var options = new DbContextOptionsBuilder<ApplicationContext>()
+                                .UseInMemoryDatabase(databaseName: _dataBaseName)
+                                .Options;
+
+                    using (var context = new ApplicationContext(options))
+                    {                        
+                        // Busca um município por id no banco de dados
+                        var municipioExistente = context.Municipios.Find(id);
+
+                        if (municipioExistente == null)
+                        {
+                            _logger.LogInformation($"Nenhum município encontrado com o ID: {id}.");
+                            return NotFound();
+                        }
+
+                        // Se existir o município na base, efetua a deleção
+                        context.Municipios.Remove(municipioExistente);
+                        context.SaveChanges();
+
+                        _logger.LogInformation($"Sucesso ao deletar o município com o ID: {id}.");
+                        return NoContent();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"Erro ao deletar o município com ID: {id} - Erro: {ex.Message} - DELETEBYID() in MunicipiosController");
                     return BadRequest(ex);
                 }
             };
